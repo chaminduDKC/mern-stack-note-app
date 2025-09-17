b61fb3a66c1e3c06ae0417d0d69c1176630c2046
ChaminduDKC <chamindud061@gmail.com>
2025-09-17 10:33:50 +0530

    second commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,22 +3,34 @@ import notesRoute from './routes/NoteRoute.js'
 import {connectDb} from "./config/db.js";
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path';
 import rateLimiter from "./middleware/rateLimiter.js";
 dotenv.config();
 const app = express();
 
 
 const port = process.env.SERVER_PORT;
+const __dirname = path.resolve();
 
 // middleware to access req body
 
 app.use(express.json());
-app.use(cors({
-    origin:"http://localhost:5173",
-}))
+
+if(process.env.NODE_ENV !== "production"){
+    app.use(cors({
+        origin:"http://localhost:5173",
+    }))
+}
 app.use(rateLimiter)
 app.use('/api/notes', notesRoute);
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res)=>{
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    })
+}
 
 // once the db connected, then server should startt
 connectDb().then(()=>{
--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+const BASE_URL = process.env.MODE === "development" ? "http://localhost:5001/api" : "/api"
 export const axiosInstance = axios.create({
-    baseURL:"http://localhost:5001/api",
+    baseURL:BASE_URL
 })
